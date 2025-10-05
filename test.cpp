e75cef8398a55c3f922d97eb97edf69d3374a3f1
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 01:48:02 +0530

    Reverse Diamond program

--- test.cpp
+++ test.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int decToBinary(int n) {
-  int ans = 0, pow = 1;
-  while (n > 0) {
-    int rem = n % 2;
-    n = n / 2;
-    ans = ans + (rem * pow);
-    pow *= 10;
-  }
-  return ans;
-}
-
 int main() {
-  int decNum;
-  cout << "enter num : ";
-  cin >> decNum;
-
-  cout << decToBinary(decNum) << endl;
+  int n;
+  cout << "enter n : ";
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < (2 * n) - (2 * i + 1); j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
   return 0;
 }
