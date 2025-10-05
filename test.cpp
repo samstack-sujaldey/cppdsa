1d316c644f78757a3b1360e35c5b6b0d5d934e63
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 02:36:43 +0530

    Half Diamond Star Pattern

--- test.cpp
+++ test.cpp
@@ -6,19 +6,13 @@ int main() {
   cout << "enter n : ";
   cin >> n;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < (n - (i + 1)); j++) {
-      cout << " ";
-    }
-    for (int j = 0; j < (2 * i + 1); j++) {
+    for (int j = 0; j <= i; j++) {
       cout << "*";
     }
     cout << endl;
   }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < i; j++) {
-      cout << " ";
-    }
-    for (int j = 0; j < (2 * n - (2 * i + 1)); j++) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - (i + 1); j++) {
       cout << "*";
     }
     cout << endl;
