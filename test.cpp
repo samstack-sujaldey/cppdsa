4fba18183c2fc75628559a255849e369f6b7dea4
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 22:04:15 +0530

    Alpha-Ramp Pattern

--- test.cpp
+++ test.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main() {
   int n;
-  cout << "enter N : ";
+  cout << "enter n : ";
   cin >> n;
-  for (int i = n; i > 0; i--) {
-    char ch = 'A';
-    for (int j = i; j > 0; j--) {
+  char ch = 'A';
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
       cout << ch << " ";
-      ch += 1;
     }
     cout << endl;
+    ch += 1;
   }
   return 0;
 }
