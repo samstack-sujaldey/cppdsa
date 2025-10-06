51d9be99d97805fbaf3776592e6a0bba831b23e4
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 21:57:13 +0530

    Reverse Letter Triangle Pattern

--- test.cpp
+++ test.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
   int n;
-  cout << "enter n : ";
+  cout << "enter N : ";
   cin >> n;
-  for (int i = 0; i < n; i++) {
+  for (int i = n; i > 0; i--) {
     char ch = 'A';
-    for (int j = 0; j <= i; j++) {
+    for (int j = i; j > 0; j--) {
       cout << ch << " ";
       ch += 1;
     }
