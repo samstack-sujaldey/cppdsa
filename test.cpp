e1b74aa56f096a369f31bd8361772d1c2b21f18c
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 21:51:58 +0530

    Increasing Letter Triangle Pattern

--- test.cpp
+++ test.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
   int n;
-  cout << "enter N : ";
+  cout << "enter n : ";
   cin >> n;
-  int num = 0;
   for (int i = 0; i < n; i++) {
+    char ch = 'A';
     for (int j = 0; j <= i; j++) {
-      num += 1;
-      cout << num << " ";
+      cout << ch << " ";
+      ch += 1;
     }
     cout << endl;
   }
