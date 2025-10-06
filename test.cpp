7f54628f7b1bbc71747411b720a35b8c8f7644f3
sujaldey004 <sujaldey2920@gmail.com>
2025-10-06 21:42:14 +0530

    Number Crown Pattern

--- test.cpp
+++ test.cpp
@@ -5,17 +5,19 @@ int main() {
   int n;
   cout << "enter n : ";
   cin >> n;
+  int spaces = 2 * (n - 1);
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= i; j++) {
-      cout << "*";
+      cout << j + 1;
+    }
+    for (int j = 0; j < spaces; j++) {
+      cout << " ";
+    }
+    for (int j = i; j >= 0; j--) {
+      cout << j + 1;
     }
     cout << endl;
-  }
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - (i + 1); j++) {
-      cout << "*";
-    }
-    cout << endl;
+    spaces -= 2;
   }
   return 0;
 }
