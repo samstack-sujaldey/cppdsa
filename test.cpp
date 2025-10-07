8a65d11a2fa4ed62522dde81a59ff12e53cce858
sujaldey004 <sujaldey2920@gmail.com>
2025-10-08 03:44:58 +0530

    Symmetric Void Pattern

--- test.cpp
+++ test.cpp
@@ -5,13 +5,29 @@ int main() {
   int n;
   cout << "enter n : ";
   cin >> n;
-  char ch = 'A';
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j <= i; j++) {
-      cout << ch << " ";
+    for (int j = n; j > i; j--) {
+      cout << "*";
+    }
+    for (int j = 0; j < i + i; j++) {
+      cout << " ";
+    }
+    for (int j = n; j > i; j--) {
+      cout << "*";
     }
     cout << endl;
-    ch += 1;
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * (n - (i + 1)); j++) {
+      cout << " ";
+    }
+    for (int j = 0; j <= i; j++) {
+      cout << "*";
+    }
+    cout << endl;
   }
   return 0;
 }
