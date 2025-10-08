142b84f48e107e6e0bcfbf8bfaffb33aeecd5a52
sujaldey004 <sujaldey2920@gmail.com>
2025-10-09 01:44:39 +0530

    The Number Pattern

--- test.cpp
+++ test.cpp
@@ -5,13 +5,14 @@ int main() {
   int n;
   cout << "enter n : ";
   cin >> n;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (i == 0 || j == 0 || j == n - 1 || i == n - 1) {
-        cout << "*";
-      } else {
-        cout << " ";
-      }
+  for (int i = 0; i < 2 * n - 1; i++) {
+    for (int j = 0; j < 2 * n - 1; j++) {
+      int top = i;
+      int bottom = j;
+      int left = (2 * n - 2) - i;
+      int right = (2 * n - 2) - j;
+
+      cout << (n - min(min(top, bottom), min(left, right)));
     }
     cout << endl;
   }
