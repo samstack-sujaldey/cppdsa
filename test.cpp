aca72cc9d1f309ea42773e86d78bd7558045fffe
sujaldey004 <sujaldey2920@gmail.com>
2025-10-08 22:29:17 +0530

    Symmetric Butterfly Pattern

--- test.cpp
+++ test.cpp
@@ -6,10 +6,27 @@ int main() {
   cout << "enter n : ";
   cin >> n;
   for (int i = 0; i < n; i++) {
-    char ch = 'A' + (n - 1);
-    ch -= i;
     for (int j = 0; j <= i; j++) {
-      cout << char(ch + j);
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * (n - (i + 1)); j++) {
+      cout << " ";
+    }
+    for (int j = 0; j <= i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < (n - i); j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * i; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < n - i; j++) {
+      cout << "*";
     }
     cout << endl;
   }
