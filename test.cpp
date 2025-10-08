b9c59d7f166bc9913e5bc8a10d90426a9a24f992
sujaldey004 <sujaldey2920@gmail.com>
2025-10-09 01:23:08 +0530

    Hollow Rectangle Pattern

--- test.cpp
+++ test.cpp
@@ -6,27 +6,12 @@ int main() {
   cout << "enter n : ";
   cin >> n;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j <= i; j++) {
-      cout << "*";
-    }
-    for (int j = 0; j < 2 * (n - (i + 1)); j++) {
-      cout << " ";
-    }
-    for (int j = 0; j <= i; j++) {
-      cout << "*";
-    }
-    cout << endl;
-  }
-
-  for (int i = 1; i < n; i++) {
-    for (int j = 0; j < (n - i); j++) {
-      cout << "*";
-    }
-    for (int j = 0; j < 2 * i; j++) {
-      cout << " ";
-    }
-    for (int j = 0; j < n - i; j++) {
-      cout << "*";
+    for (int j = 0; j < n; j++) {
+      if (i == 0 || j == 0 || j == n - 1 || i == n - 1) {
+        cout << "*";
+      } else {
+        cout << " ";
+      }
     }
     cout << endl;
   }
