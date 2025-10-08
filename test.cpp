9bbedcf5a5cd799c3e5418d684b43b90866d38a7
sujaldey004 <sujaldey2920@gmail.com>
2025-10-08 20:08:37 +0530

    Alpha Hill Pattern

--- test.cpp
+++ test.cpp
@@ -3,29 +3,22 @@ using namespace std;
 
 int main() {
   int n;
-  cout << "enter n : ";
+  cout << "enter N : ";
   cin >> n;
   for (int i = 0; i < n; i++) {
-    for (int j = n; j > i; j--) {
-      cout << "*";
-    }
-    for (int j = 0; j < i + i; j++) {
+    for (int j = 0; j < n - (i + 1); j++) {
       cout << " ";
     }
-    for (int j = n; j > i; j--) {
-      cout << "*";
-    }
-    cout << endl;
-  }
-  for (int i = 0; i < n; i++) {
+    char ch = 'A';
     for (int j = 0; j <= i; j++) {
-      cout << "*";
+      cout << ch;
+      ch += 1;
     }
-    for (int j = 0; j < 2 * (n - (i + 1)); j++) {
-      cout << " ";
-    }
-    for (int j = 0; j <= i; j++) {
-      cout << "*";
+    for (int j = 1; j <= i; j++) {
+      char ch = 'A';
+      if (i >= 1) {
+        cout << char(ch + (i - j));
+      }
     }
     cout << endl;
   }
