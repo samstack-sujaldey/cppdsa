59bd1ab9333029ba463f4af58249850d3974a4d8
sujaldey004 <sujaldey2920@gmail.com>
2025-10-11 20:29:12 +0530

    Palindrome check by stack

--- test.cpp
+++ test.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
 int main() {
-  int days;
-  cout << "enter day no. : ";
-  cin >> days;
-  switch (days) {
-  case 1:
-    cout << "Monday" << endl;
-    break;
-  case 2:
-    cout << "Tuesday" << endl;
-    break;
-  case 3:
-    cout << "Wednesday" << endl;
-    break;
-  case 4:
-    cout << "Thursday" << endl;
-    break;
-  case 5:
-    cout << "Friday" << endl;
-    break;
-  case 6:
-    cout << "Saturday" << endl;
-    break;
-  case 7:
-    cout << "Sunday" << endl;
-    break;
-  default:
-    cout << "Invalid" << endl;
-    break;
+  int n;
+  cout << "enter n : ";
+  cin >> n;
+  stack<int> stk;
+  int rev = 0;
+  while (n != 0) {
+    int ld = n % 10;
+    stk.push(ld);
+    n /= 10;
+    rev = rev * 10 + ld;
+  }
+  string chk2 = to_string(rev);
+  bool check = true;
+  int i = 0;
+  while (!stk.empty() && check) {
+    if (stk.top() != chk2[i] - '0') {
+      check = false;
+      break;
+    }
+    stk.pop();
+    i++;
+  }
+  if (check) {
+    cout << "is palindrome" << endl;
+  } else {
+    cout << "not palindrome" << endl;
   }
   return 0;
 }
