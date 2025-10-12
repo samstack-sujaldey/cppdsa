6b5161802cfdc57271bdfd79016b8e8e0aa768cc
sujaldey004 <sujaldey2920@gmail.com>
2025-10-13 03:41:08 +0530

    Reverse Array - Using recursion

--- test.cpp
+++ test.cpp
@@ -1,29 +1,32 @@
-#include <algorithm>
 #include <iostream>
+#include <utility>
 using namespace std;
+
+void reverseArray(int arr[], int start, int end) {
+  if (start < end) {
+    swap(arr[start], arr[end]);
+    reverseArray(arr, start + 1, end - 1);
+  }
+}
 
 int main() {
   int n;
   cout << "enter n : ";
   cin >> n;
+
   int arr[n];
+
   cout << "enter array elements : ";
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-  int l = 0;
-  int r = n - 1;
-  while (l < r) {
-    int temp;
-    temp = arr[l];
-    arr[l] = arr[r];
-    arr[r] = temp;
-    l++;
-    r--;
-  }
+
+  reverseArray(arr, 0, n - 1);
+
   for (auto e : arr) {
     cout << e << " ";
   }
   cout << endl;
+
   return 0;
 }
