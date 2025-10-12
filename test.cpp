b0efa1533beae845c6bd9c0bc7078becec9a26bc
sujaldey004 <sujaldey2920@gmail.com>
2025-10-13 03:02:35 +0530

    factorial of N using recursion

--- test.cpp
+++ test.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int fact(int n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+}
+
 int main() {
   int n;
   cout << "enter n : ";
   cin >> n;
-  int fact = 1;
-  for (int i = 1; i <= n; i++) {
-    fact *= i;
-  }
-  cout << fact << endl;
+  cout << fact(n) << endl;
   return 0;
 }
