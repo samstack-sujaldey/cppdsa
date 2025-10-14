3fa993f0f87832a4dadd04d63b78581bf828264b
sujaldey004 <sujaldey2920@gmail.com>
2025-10-14 11:42:43 +0530

    Tower of Hanoi Problem
    Very important interview problem Difficulty level - Medim

--- test.cpp
+++ test.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int largest(int arr[], int l, int r) {
-  int max;
-  if (l == r) {
-    return l;
-  } else {
-    max = largest(arr, l + 1, r);
-    if (arr[l] > max) {
-      return arr[l];
-    } else {
-      return max;
-    }
-  }
-}
+void towerOFHanoi(int numOFDisks, char needle1, char needle3, char needle2);
 
 int main() {
   int n;
+  cout << "enter n : ";
   cin >> n;
-  int arr[n];
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
-  }
-  cout << largest(arr, 0, n - 1) << endl;
+  towerOFHanoi(n, 'A', 'B', 'C');
   return 0;
+}
+
+void towerOFHanoi(int numOFDisks, char needle1, char needle3, char needle2) {
+  if (numOFDisks > 0) {
+    towerOFHanoi(numOFDisks - 1, needle1, needle2, needle3);
+    cout << "Move disk " << numOFDisks << " from " << needle1 << " to "
+         << needle3 << "." << endl;
+    towerOFHanoi(numOFDisks - 1, needle2, needle3, needle1);
+  }
 }
