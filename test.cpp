678f8c7e162a8258084c60960782800c1f6d7bc7
sujaldey004 <sujaldey2920@gmail.com>
2025-10-14 09:51:43 +0530

    character hashing
    for lowercase alphabet

--- test.cpp
+++ test.cpp
@@ -2,26 +2,21 @@
 using namespace std;
 
 int main() {
+  string s;
+  cin >> s;
+
+  // pre - compute
+  int hash[26] = {0};
+  for (int i = 0; i < s.size(); i++) {
+    hash[s[i] - 'a']++;
+  }
+
   int n;
   cin >> n;
-  int arr[n];
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
+  while (n--) {
+    char ch;
+    cin >> ch;
+    cout << hash[ch - 'a'] << endl;
   }
-
-  // pre-storing
-  int hash[13] = {0};
-  for (int i = 0; i < n; i++) {
-    hash[arr[i]] += 1;
-  }
-
-  int q;
-  cin >> q;
-  while (q--) {
-    int queries;
-    cin >> queries;
-    cout << hash[queries] << " ";
-  }
-  cout << endl;
   return 0;
 }
