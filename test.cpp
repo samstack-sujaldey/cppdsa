703dfc15fbdb643f82d6146e5ff00f27c0e1a90d
sujaldey004 <sujaldey2920@gmail.com>
2025-10-14 10:08:36 +0530

    Largest element in array using recursion

--- test.cpp
+++ test.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  string s;
-  cin >> s;
-
-  // pre - compute
-  int hash[26] = {0};
-  for (int i = 0; i < s.size(); i++) {
-    hash[s[i] - 'a']++;
+int largest(int arr[], int l, int r) {
+  int max;
+  if (l == r) {
+    return l;
+  } else {
+    max = largest(arr, l + 1, r);
+    if (arr[l] > max) {
+      return arr[l];
+    } else {
+      return max;
+    }
   }
+}
 
+int main() {
   int n;
   cin >> n;
-  while (n--) {
-    char ch;
-    cin >> ch;
-    cout << hash[ch - 'a'] << endl;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
   }
+  cout << largest(arr, 0, n - 1) << endl;
   return 0;
 }
