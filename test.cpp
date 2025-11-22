4cd9461459850c781a7a66357a5a87011084606b
sujaldey004 <sujaldey2920@gmail.com>
2025-11-22 21:22:13 +0530

    counting and printing frequencies of element of array

--- test.cpp
+++ test.cpp
@@ -5,24 +5,20 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
-  cin >> n;
-  vector<int> arr(n);
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i];
+  vector<int> arr;
+  int x;
+
+  while (cin >> x) {
+    arr.push_back(x);
   }
 
   unordered_map<int, int> mp;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < arr.size(); i++) {
     mp[arr[i]]++;
   }
 
-  int q;
-  cin >> q;
-  while (q--) {
-    int num;
-    cin >> num;
-    cout << mp[num] << endl;
+  for (auto e : mp) {
+    cout << e.first << " -> " << e.second << endl;
   }
   return 0;
 }
