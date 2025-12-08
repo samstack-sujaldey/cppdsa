f0e326febf034cd951ababe6d4b16e7b19be500e
sujaldey004 <sujaldey2920@gmail.com>
2025-12-08 21:21:09 +0530

    Insertion sort

--- test.cpp
+++ test.cpp
@@ -11,20 +11,19 @@ int main() {
     arr.push_back(x);
   }
 
-  for (int i = arr.size() - 1; i >= 0; i--) {
-    for (int j = 0; j < i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+  for (int i = 0; i < arr.size(); i++) {
+    int j = i;
+
+    while (j > 0 && arr[j - 1] < arr[j]) {
+      int temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j--;
     }
   }
-
   for (auto e : arr) {
     cout << e << " ";
   }
-
   cout << endl;
   return 0;
 }
